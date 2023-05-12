86c71f633a4e8f8b3e152b0a22929c892b170a0d
gerardoMundo <ger.mundo66@gmail.com>
2023-05-12 17:16:07 -0600

    New page template finished

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { HeroService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
+  public hero?: Hero;
 
+  ngOnInit(): void {
+    this.activateRoute.params
+      .pipe(switchMap(({ id }) => this.heroService.getHeroById(id)))
+      .subscribe((hero) => {
+        if (!hero) return this.router.navigate(['/heroes/list']);
+        this.hero = hero;
+        return;
+      });
+  }
+
+  constructor(
+    private heroService: HeroService,
+    private activateRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  goBack(): void {
+    this.router.navigateByUrl('heroes/list');
+  }
 }
